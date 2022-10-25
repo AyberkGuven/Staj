f865aabfd79733fbb836426116d6377c60b6eacd
Ayberk Güven <90345997+AyberkGuven@users.noreply.github.com>
2022-10-26 00:20:46 +0300

    Add files via upload

---
+++ Staj/Staj/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Staj.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string userName { get; set; }
+        public string Password { get; set; }
+        public bool Role { get; set; }
+    }
+}
